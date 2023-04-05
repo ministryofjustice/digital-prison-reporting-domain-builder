c5428f4297d9ad58550d352f5764106dc59e3caa
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-04-05 12:14:17 +0100

    DPR-332 clean up domain builder code and add history file support

--- cli/src/main/kotlin/uk/gov/justice/digital/DomainBuilder.kt
+++ cli/src/main/kotlin/uk/gov/justice/digital/DomainBuilder.kt
@@ -7,22 +7,22 @@ import org.fusesource.jansi.AnsiConsole
 import org.jline.console.SystemRegistry
 import org.jline.console.impl.SystemRegistryImpl
 import org.jline.reader.*
+import org.jline.reader.LineReader.*
 import org.jline.reader.impl.DefaultParser
 import org.jline.terminal.Terminal
 import org.jline.terminal.TerminalBuilder
 import org.jline.widget.AutosuggestionWidgets
 import picocli.CommandLine
-import picocli.CommandLine.*
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
 import picocli.shell.jline3.PicocliCommands
 import uk.gov.justice.digital.command.CommandBase
 import uk.gov.justice.digital.command.ListDomains
 import uk.gov.justice.digital.command.ViewDomain
-import java.io.PrintWriter
 
 @Command(
     name = "domain-builder",
     subcommands = [
-        HelpCommand::class,
         ListDomains::class,
         ViewDomain::class,
     ],
@@ -32,52 +32,20 @@ class DomainBuilder : CommandBase(), Runnable {
 
     @Option(
         names = ["-i", "--interactive"],
-        description = ["Run the command in interactive mode"],
+        description = ["Run domain-builder in interactive mode"],
         required = false
     )
     var interactive = false
 
-    @Option(
-        names = ["-h", "--help"],
-        usageHelp = true,
-        description = [ "display this help message" ]
-    )
-    var usageHelpRequested = false
-
-    private var out: PrintWriter? = null
-
-    private fun setReader(reader: LineReader) {
-        out = reader.terminal.writer()
-    }
-
-    private val launchText = """
-        
-        @|bold,cyan  ____                                  ____          _     _               |@
-        @|bold,cyan |  _ \  ___  _ __ ___   __ _(_)_ __   | __ ) _   _(_) | __| | ___ _ __     |@
-        @|bold,cyan | | | |/ _ \| '_ ` _ \ / _` | | '_ \  |  _ \| | | | | |/ _` |/ _ \ '__|    |@
-        @|bold,cyan | |_| | (_) | | | | | | (_| | | | | | | |_) | |_| | | | (_| |  __/ |       |@
-        @|bold,cyan |____/ \___/|_| |_| |_|\__,_|_|_| |_| |____/ \__,_|_|_|\__,_|\___|_|       |@
-
-        Type @|bold help|@ to view available commands.
-        
-        Type @|bold <command> --help|@ to view help for a specific command.
-        
-        Type @|bold exit|@ to exit the domain builder.
-        
-        Press the @|bold TAB|@ key to view available commands or autocomplete commands as you type.
-        
-    """.trimIndent()
-
     override fun run() {
         if (interactive) {
+            AnsiConsole.systemInstall()
             printlnAnsi(launchText)
 
-            AnsiConsole.systemInstall()
-
             try {
-                TerminalBuilder.builder().build().use { terminal -> interactiveSession(terminal) }
-            } catch (t: Throwable) {
-                t.printStackTrace()
+                TerminalBuilder.builder()
+                    .build()
+                    .use(this::interactiveSession)
             } finally {
                 AnsiConsole.systemUninstall()
             }
@@ -85,46 +53,45 @@ class DomainBuilder : CommandBase(), Runnable {
     }
 
     private fun interactiveSession(terminal: Terminal) {
-        // Set up picocli commands
-        val commands = this
-        val factory = MicronautFactory()
-        val cmd = CommandLine(commands, factory)
-        val picocliCommands = PicocliCommands(cmd)
+        val commandLine = CommandLine(this, MicronautFactory())
+
         val parser: Parser = DefaultParser()
         val systemRegistry: SystemRegistry = SystemRegistryImpl(parser, terminal, null, null)
 
-        systemRegistry.setCommandRegistries(picocliCommands)
-        systemRegistry.register("help", picocliCommands)
+        systemRegistry.setCommandRegistries(PicocliCommands(commandLine))
 
         val reader = LineReaderBuilder.builder()
             .terminal(terminal)
             .completer(systemRegistry.completer())
             .parser(parser)
-            .variable(LineReader.LIST_MAX, 50) // max tab completion candidates
+            .variable(LIST_MAX, tabCompletionMaxCandidates)
+            .variable(HISTORY_FILE, historyFileLocation)
+            .variable(HISTORY_SIZE, historySize)
+            .variable(HISTORY_FILE_SIZE, historySize)
             .build()
-        commands.setReader(reader)
 
+        // Enable tab completion.
         AutosuggestionWidgets(reader).enable()
 
-        val prompt = "domain-builder> "
-
-        // Start the shell and process input until the user quits with Ctrl-D
+        // Start the interactive shell and process input until the user types exit or hits CTRL-D.
         while (true) {
             try {
                 systemRegistry.cleanUp()
-                systemRegistry.execute(reader.readLine(prompt, null, null))
-            } catch (e: UserInterruptException) {
-                // Ignore
-            } catch (e: EndOfFileException) {
-                return
-            } catch (e: Exception) {
+                systemRegistry.execute(reader.readLine(prompt))
+            }
+            catch (e: EndOfFileException) {
+                break
+            }
+            catch (e: Exception) {
                 systemRegistry.trace(e)
+                break
             }
         }
 
     }
 
     companion object {
+
         @JvmStatic
         fun main(args: Array<String>) {
             System.setProperty("picocli.ansi", "true")
@@ -135,6 +102,30 @@ class DomainBuilder : CommandBase(), Runnable {
             }
             else PicocliRunner.execute(DomainBuilder::class.java, *args)
         }
+
+        private const val prompt = "domain-builder> "
+        private const val tabCompletionMaxCandidates = 50
+        private val historyFileLocation = "${System.getProperty("user.home")}/.domain-builder_history"
+        private const val historySize = 100
+
+        private val launchText = """
+        
+            @|bold,cyan  ____                                  ____          _     _               |@
+            @|bold,cyan |  _ \  ___  _ __ ___   __ _(_)_ __   | __ ) _   _(_) | __| | ___ _ __     |@
+            @|bold,cyan | | | |/ _ \| '_ ` _ \ / _` | | '_ \  |  _ \| | | | | |/ _` |/ _ \ '__|    |@
+            @|bold,cyan | |_| | (_) | | | | | | (_| | | | | | | |_) | |_| | | | (_| |  __/ |       |@
+            @|bold,cyan |____/ \___/|_| |_| |_|\__,_|_|_| |_| |____/ \__,_|_|_|\__,_|\___|_|       |@
+    
+            Type @|bold help|@ to view available commands.
+            
+            Type @|bold <command> --help|@ to view help for a specific command.
+            
+            Type @|bold exit|@ to exit the domain builder.
+            
+            Press the @|bold TAB|@ key to view available commands or autocomplete commands as you type.
+        
+        """.trimIndent()
+
     }
 
 }
