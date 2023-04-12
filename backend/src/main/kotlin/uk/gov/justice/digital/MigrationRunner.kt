93c52d9e574217601e5ae8520ebf15d0f9f9c88a
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-04-12 10:59:53 +0100

    DPR-362 added apply-migrations script with error handling

--- backend/src/main/kotlin/uk/gov/justice/digital/MigrationRunner.kt
+++ backend/src/main/kotlin/uk/gov/justice/digital/MigrationRunner.kt
@@ -4,20 +4,37 @@ import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.flyway.FlywayConfigurationProperties
 import io.micronaut.flyway.FlywayMigrator
 import jakarta.inject.Singleton
+import org.flywaydb.core.Flyway
 import picocli.CommandLine.Command
 import javax.sql.DataSource
+import kotlin.system.exitProcess
 
+/**
+ * Command to apply migrations on the configured database.
+ *
+ * This can be used to apply migrations locally or as part of a deployment pipeline.
+ *
+ * See script ./bin/apply-migrations which executes this command class.
+ */
 @Singleton
 @Command(name = "MigrationRunner")
-class MigrationRunner(
-    private val flywayMigrator: FlywayMigrator,
-    private val flywayConfig: FlywayConfigurationProperties,
-    private val defaultDataSource: DataSource,
-) : Runnable {
+class MigrationRunner(private val defaultDataSource: DataSource) : Runnable {
 
     override fun run() {
         println("Applying migrations")
-        flywayMigrator.run(flywayConfig, defaultDataSource)
+        try {
+            Flyway
+                .configure()
+                .dataSource(defaultDataSource)
+                .load()
+                .migrate()
+        } catch (e: Exception) {
+            println("Migrations failed to apply")
+            println("Error: $e")
+            println("Stacktrace:")
+            e.printStackTrace()
+            exitProcess(1)
+        }
         println("Migrations applied successfully")
     }
 
