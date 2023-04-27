85c1b50c6cec4bf2adb3c3bf6a37f60d4e794c4c
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-04-27 16:34:27 +0100

    DPR-407 run ps aux to see if anything useful is running

--- backend/src/test/kotlin/uk/gov/justice/digital/repository/TemporaryTest.kt
+++ backend/src/test/kotlin/uk/gov/justice/digital/repository/TemporaryTest.kt
@@ -17,6 +17,17 @@ class TemporaryTest {
     }
 
     @Test
+    fun `show running processes`() {
+        println("Running ps aux to see if there is anything docker like running")
+        val result = ProcessBuilder("ps", "aux")
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start()
+            .waitFor()
+        assertEquals(0, result)
+    }
+
+    @Test
     fun `docker ps returns successfully`() {
         println("Running docker ps to determine if it is up or not")
         val result = ProcessBuilder("docker", "ps")
