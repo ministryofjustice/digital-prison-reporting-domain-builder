b637cd19e385e750a41ff6f2d43df3f3ef84da45
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-06-26 14:44:18 +0100

    DPR-544 add aws lambda handler for invocation as an aws lambda function

--- backend/src/main/kotlin/uk/gov/justice/digital/backend/MigrationRunner.kt
+++ backend/src/main/kotlin/uk/gov/justice/digital/backend/MigrationRunner.kt
@@ -1,8 +1,11 @@
 package uk.gov.justice.digital.backend
 
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
 import io.micronaut.configuration.picocli.PicocliRunner
 import jakarta.inject.Singleton
 import org.flywaydb.core.Flyway
+import org.slf4j.LoggerFactory
 import picocli.CommandLine.Command
 import javax.sql.DataSource
 import kotlin.system.exitProcess
@@ -10,23 +13,20 @@ import kotlin.system.exitProcess
 /**
  * Command to apply migrations on the configured database.
  *
- * This can be used to apply migrations locally or as part of a deployment pipeline.
+ * This can be used to apply migrations locally or as part of a deployment pipeline as an AWS Lambda function.
  *
  * See script ./bin/apply-migrations which executes this command class.
  */
 @Singleton
 @Command(name = "MigrationRunner")
-class MigrationRunner(private val defaultDataSource: DataSource) : Runnable {
+class MigrationRunner(private val defaultDataSource: DataSource) : Runnable, RequestHandler<Unit, Unit> {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    // Run method for conventional execution via main method with output sent to STDOUT.
     override fun run() {
         println("Applying migrations")
-        try {
-            Flyway
-                .configure()
-                .dataSource(defaultDataSource)
-                .load()
-                .migrate()
-        } catch (e: Exception) {
+        runMigrationsWithExceptionHandler { e ->
             println("Migrations failed to apply")
             println("Error: $e")
             println("Stacktrace:")
@@ -35,6 +35,27 @@ class MigrationRunner(private val defaultDataSource: DataSource) : Runnable {
         }
         println("Migrations applied successfully")
     }
+
+    // AWS Lambda request handler for execution on AWS as a Lambda Function.
+    override fun handleRequest(input: Unit?, context: Context?) {
+        logger.info("Applying migrations")
+        runMigrationsWithExceptionHandler { e ->
+            logger.error("Failed to apply migrations", e)
+            throw e
+        }
+        logger.info("Migrations applied successfully")
+    }
+
+    private fun runMigrationsWithExceptionHandler(handleException: (Exception) -> Unit) {
+        try {
+            Flyway
+                .configure()
+                .dataSource(defaultDataSource)
+                .load()
+                .migrate()
+        } catch (e: Exception) { handleException(e) }
+    }
+
 
     companion object {
         @JvmStatic
