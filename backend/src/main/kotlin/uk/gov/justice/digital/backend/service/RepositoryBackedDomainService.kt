3953e9b6c46d4e8a85d287907cac380c6e4bcbbf
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-06-29 17:25:36 +0100

    DPR-411 initiial integration of sql validator into service with tests

--- backend/src/main/kotlin/uk/gov/justice/digital/backend/service/RepositoryBackedDomainService.kt
+++ backend/src/main/kotlin/uk/gov/justice/digital/backend/service/RepositoryBackedDomainService.kt
@@ -2,9 +2,12 @@ package uk.gov.justice.digital.backend.service
 
 import jakarta.inject.Singleton
 import uk.gov.justice.digital.backend.repository.DomainRepository
+import uk.gov.justice.digital.backend.validator.InvalidSparkSql
+import uk.gov.justice.digital.backend.validator.SparkSqlValidator
 import uk.gov.justice.digital.model.Domain
 import uk.gov.justice.digital.model.Status
 import uk.gov.justice.digital.model.WriteableDomain
+import java.lang.RuntimeException
 import java.util.*
 
 interface DomainService {
@@ -21,8 +24,23 @@ interface DomainService {
  * Update and delete to follow in later work.
  */
 @Singleton
-class RepositoryBackedDomainService(private val repository: DomainRepository): DomainService {
+class RepositoryBackedDomainService(
+    private val repository: DomainRepository,
+    private val sqlValidator: SparkSqlValidator): DomainService {
+
     override fun getDomains(name: String?, status: Status?): List<Domain> = repository.getDomains(name)
+
     override fun getDomain(id: UUID): Domain? = repository.getDomain(id)
-    override fun createDomain(domain: WriteableDomain): UUID = repository.createDomain(domain)
+
+    override fun createDomain(domain: WriteableDomain): UUID {
+        // Map list of tables and validate each mapping so we get a list of validation results
+        val results = domain.tables
+            .map { it.mapping.viewText }
+            .map { sqlValidator.validate(it) }
+            .filterNot { it.isValid }
+
+        if (results.isEmpty()) return repository.createDomain(domain)
+        // TODO - tidy this up
+        else throw RuntimeException((results.first() as InvalidSparkSql).reason)
+    }
 }
---
+++ backend/src/test/kotlin/uk/gov/justice/digital/backend/service/RepositoryBackedDomainServiceTest.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.backend.service
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertThrows
+import uk.gov.justice.digital.backend.repository.DomainRepository
+import uk.gov.justice.digital.backend.validator.InvalidSparkSql
+import uk.gov.justice.digital.backend.validator.SparkSqlValidator
+import uk.gov.justice.digital.backend.validator.ValidSparkSql
+import uk.gov.justice.digital.model.WriteableDomain
+import uk.gov.justice.digital.test.Fixtures.mapping
+import uk.gov.justice.digital.test.Fixtures.writeableDomain
+import java.util.*
+
+class RepositoryBackedDomainServiceTest {
+
+    private val mockRepository: DomainRepository = mockk()
+    private val mockValidator: SparkSqlValidator = mockk()
+    private val fixedUUID = UUID.randomUUID()
+
+    private val underTest = RepositoryBackedDomainService(mockRepository, mockValidator)
+
+    @Test
+    fun `create should create a domain containing valid spark sql`() {
+        every { mockRepository.createDomain(any<WriteableDomain>()) } returns fixedUUID
+        every { mockValidator.validate(any()) } returns ValidSparkSql()
+        val result = underTest.createDomain(writeableDomain)
+        assertEquals(fixedUUID, result)
+    }
+
+    @Test
+    fun `create should throw an exception given a domain containing invalid spark sql`() {
+        every { mockValidator.validate(any()) } returns InvalidSparkSql("Parse failure message")
+
+        val writeableDomainWithInvalidSql = writeableDomain.copy(
+            tables = writeableDomain.tables.map {
+                it.copy(mapping = mapping.copy(viewText = "This is not valid SQL"))
+            }
+        )
+
+        assertThrows(RuntimeException::class.java) {
+            underTest.createDomain(writeableDomainWithInvalidSql)
+        }
+    }
+
+}
