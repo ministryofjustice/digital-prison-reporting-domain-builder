0cad1af78a0a36bd6afe6350f6cede2862262606
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-07-04 15:22:31 +0100

    DPR-594 remove deps across all configurations to save repetition

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,34 @@ val ktormVersion = "3.6.0"
 val testContainersVersion = "1.18.0"
 val sparkVersion = "3.3.0"
 
+configurations {
+    all {
+      exclude("com.google.crypto.tink")
+      exclude("com.google.flatbuffers")
+      exclude("com.google.protobuf")
+      exclude("com.twitter")
+      exclude("org.apache.arrow")
+      exclude("org.apache.avro")
+      exclude("org.apache.curator", "curator-recipes")
+      exclude("org.apache.hadoop", "hadoop-client")
+      exclude("org.apache.hadoop", "hadoop-client-runtime")
+      exclude("org.apache.hive")
+      exclude("org.apache.ivy")
+      exclude("org.apache.orc")
+      exclude("org.apache.parquet")
+      exclude("org.apache.spark", "spark-kvstore_2.12")
+      exclude("org.apache.spark", "spark-launcher_2.12")
+      exclude("org.apache.spark", "spark-network-shuffle_2.12")
+      exclude("org.apache.spark", "spark-sketch_2.12")
+      exclude("org.apache.spark", "spark-tags_2.12")
+      exclude("org.apache.spark", "spark-unsafe_2.12")
+      exclude("org.apache.zookeeper", "zookeeper")
+      exclude("org.glassfish.jersey.containers")
+      exclude("org.glassfish.jersey.core")
+      exclude("org.glassfish.jersey.inject")
+    }
+}
+
 dependencies {
   implementation(project(":common"))
   testImplementation(testFixtures(project(":common")))
@@ -50,43 +78,9 @@ dependencies {
   implementation("org.postgresql:postgresql:42.6.0")
 
   // Spark dependencies
-  implementation("org.apache.spark:spark-sql_2.12:$sparkVersion") {
-    // Exclude transitive dependencies that are not needed to keep the unpacked
-    // jar size within the AWS Lambda limit of 250MiB
-    exclude("org.apache.spark", "spark-core_2.12")
-    exclude("org.apache.spark", "spark-catalyst_2.12")
-    exclude("org.apache.spark", "spark-sketch_2.12")
-    exclude("org.apache.orc")
-    exclude("org.apache.parquet")
-  }
-
-  implementation("org.apache.spark:spark-core_2.12:$sparkVersion") {
-    // Exclude transitive dependencies that are not needed to keep the unpacked
-    // jar size within the AWS Lambda limit of 250MiB
-    exclude("io.netty")
-    exclude("org.apache.spark", "spark-catalyst_2.12")
-    exclude("org.apache.spark", "spark-kvstore_2.12")
-    exclude("org.apache.spark", "spark-launcher_2.12")
-    exclude("org.apache.spark", "spark-network-shuffle_2.12")
-    exclude("org.apache.spark", "spark-sketch_2.12")
-    exclude("org.apache.spark", "spark-unsafe_2.12")
-    exclude("org.apache.avro", "avro-mapred")
-    exclude("org.apache.curator", "curator-recipes")
-    exclude("org.apache.zookeeper", "zookeeper")
-    exclude("org.glassfish.jersey.core")
-    exclude("org.glassfish.jersey.containers")
-    exclude("org.glassfish.jersey.inject")
-    exclude("com.twitter")
-  }
-
-  implementation("org.apache.spark:spark-catalyst_2.12:$sparkVersion") {
-    exclude("org.apache.spark", "spark-unsafe_2.12")
-    exclude("com.twitter")
-    exclude("org.apache.arrow")
-    exclude("org.apache.orc")
-    exclude("org.apache.hive")
-    exclude("org.apache.parquet")
-  }
+  implementation("org.apache.spark:spark-catalyst_2.12:$sparkVersion")
+  implementation("org.apache.spark:spark-core_2.12:$sparkVersion")
+  implementation("org.apache.spark:spark-sql_2.12:$sparkVersion")
 
   kapt("io.micronaut:micronaut-inject-java")
   kapt("io.micronaut:micronaut-http-validation")
