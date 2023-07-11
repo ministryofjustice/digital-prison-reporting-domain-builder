d1ae6e692448d8d8b395f5d80e948db3292a9747
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-07-11 10:45:17 +0100

    NOJIRA ensure WriteableDomain aligns to Domain

--- common/src/main/kotlin/uk/gov/justice/digital/model/WriteableDomain.kt
+++ common/src/main/kotlin/uk/gov/justice/digital/model/WriteableDomain.kt
@@ -7,12 +7,12 @@ import io.micronaut.serde.annotation.Serdeable
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class WriteableDomain(
         val name: String,
-        val description: String,
-        val version: String,
-        val location: String,
+        val description: String?,
+        val version: String?,
+        val location: String?,
         val tags: Map<String, String> = emptyMap(),
-        val owner: String,
-        val author: String,
+        val owner: String?,
+        val author: String?,
         val tables: List<Table> = emptyList(),
         // The following fields are domain builder specific and can be omitted from the published domain.
         val status: Status = Status.DRAFT,
