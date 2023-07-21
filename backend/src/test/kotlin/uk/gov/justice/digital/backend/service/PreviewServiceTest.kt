8f02a94c73fdf342d56f382cc4b6ac297e1175e6
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-07-21 16:29:59 +0100

    DPR-611 added tests covering the exception cases

--- backend/src/test/kotlin/uk/gov/justice/digital/backend/service/PreviewServiceTest.kt
+++ backend/src/test/kotlin/uk/gov/justice/digital/backend/service/PreviewServiceTest.kt
@@ -4,6 +4,7 @@ import io.mockk.every
 import io.mockk.mockk
 import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.postgresql.ds.PGSimpleDataSource
@@ -12,8 +13,13 @@ import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import uk.gov.justice.digital.backend.repository.DomainRepository
+import uk.gov.justice.digital.model.Domain
 import uk.gov.justice.digital.model.Status
+import uk.gov.justice.digital.model.Table
+import uk.gov.justice.digital.model.Transform
 import uk.gov.justice.digital.test.Fixtures.domain1
+import uk.gov.justice.digital.test.Fixtures.domain2
+import uk.gov.justice.digital.test.Fixtures.table1
 
 @Testcontainers
 class PreviewServiceTest {
@@ -52,11 +58,9 @@ class PreviewServiceTest {
     private val underTest by lazy { PreviewService(dataSource, mockRepository) }
 
     @Test
-    fun `it should return the query result as a list of maps`() {
-        val domainWithTestDomainQuery = domain1.copy(
-            tables = domain1.tables.map {
-                it.copy(transform = it.transform.copy(viewText = "select * from test_domain"))
-            }
+    fun `it should execute the query and return the results for a valid request`() {
+        val domainWithTestDomainQuery =  domain1.withTables(
+            listOf(table1.copy(transform = Transform("select * from test_domain", emptyList())))
         )
 
         every { mockRepository.getDomains(any(), any()) } answers { listOf(domainWithTestDomainQuery) }
@@ -75,4 +79,25 @@ class PreviewServiceTest {
 
         assertEquals(expectedFirstRow, result[0])
     }
+
+    @Test
+    fun `it should throw a DomainNotFoundException if no matching domains are returned`() {
+        every { mockRepository.getDomains(any(), any()) } answers { emptyList() }
+        assertThrows(DomainNotFoundException::class.java) { underTest.preview("Test Domain", Status.DRAFT, 50) }
+    }
+
+    @Test
+    fun `it should throw a MultipleDomainsFoundException if more than one matching domain is returned`() {
+        every { mockRepository.getDomains(any(), any()) } answers { listOf(domain1 ,domain2) }
+        assertThrows(MultipleDomainsFoundException::class.java) { underTest.preview("Test Domain", Status.DRAFT, 50) }
+    }
+
+    @Test
+    fun `it should throw a NoTablesInDomainException if the domain has no tables defined`() {
+        every { mockRepository.getDomains(any(), any()) } answers { listOf(domain1.withTables(emptyList())) }
+        assertThrows(NoTablesInDomainException::class.java) { underTest.preview("Test Domain", Status.DRAFT, 50) }
+    }
+
+    private fun Domain.withTables(tables: List<Table>) = this.copy(tables = tables)
+
 }
