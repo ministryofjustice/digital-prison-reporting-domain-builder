e1410bd42d4571073345c3ae39ea5cca15c7595f
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-08-01 11:01:52 +0100

    DPR-612 revised preview test now we fetch the terminal height

--- cli/src/test/kotlin/uk/gov/justice/digital/cli/command/PreviewDomainTest.kt
+++ cli/src/test/kotlin/uk/gov/justice/digital/cli/command/PreviewDomainTest.kt
@@ -2,10 +2,12 @@ package uk.gov.justice.digital.cli.command
 
 import io.mockk.every
 import io.mockk.mockk
+import org.jline.terminal.Terminal
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.cli.DomainBuilder
 import uk.gov.justice.digital.cli.service.DomainService
+import uk.gov.justice.digital.cli.session.InteractiveSession
 import uk.gov.justice.digital.model.Status
 import uk.gov.justice.digital.test.Fixtures.domainPreviewData
 
@@ -13,6 +15,8 @@ class PreviewDomainTest {
 
     private val mockDomainBuilder: DomainBuilder = mockk()
     private val mockDomainService: DomainService = mockk()
+    private val mockInteractiveSession: InteractiveSession = mockk()
+    private val mockTerminal: Terminal = mockk()
 
     private val underTest = PreviewDomain(mockDomainService)
 
@@ -25,7 +29,13 @@ class PreviewDomainTest {
         underTest.domainNameElements = arrayOf("Domain 1")
         underTest.domainStatus = Status.DRAFT
 
+        every { mockInteractiveSession.isInteractive() } answers { true }
+        every { mockInteractiveSession.terminal() } answers {mockTerminal }
+        every { mockTerminal.height } answers { 25 }
+
         every { mockDomainBuilder.print(capture(capturedOutput)) } answers {  }
+        every { mockDomainBuilder.session() } answers { mockInteractiveSession }
+        every { mockDomainBuilder.getInteractiveSession() } answers { mockInteractiveSession }
         every { mockDomainService.previewDomain(any(), any(), any()) } answers { domainPreviewData }
 
         underTest.run()
