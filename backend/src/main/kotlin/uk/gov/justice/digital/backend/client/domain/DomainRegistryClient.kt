dffd99321ca934eb6973d9fdf7ab284659faf681
Carwyn Ellis <125277169+carwynmoj@users.noreply.github.com>
2023-08-09 16:24:44 +0100

    DPR-643 integrate dynamo publish logic into main client code

--- backend/src/main/kotlin/uk/gov/justice/digital/backend/client/domain/DomainRegistryClient.kt
+++ backend/src/main/kotlin/uk/gov/justice/digital/backend/client/domain/DomainRegistryClient.kt
@@ -1,106 +1,112 @@
 package uk.gov.justice.digital.backend.client.domain
 
-import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.model.*
 import com.amazonaws.services.dynamodbv2.model.ComparisonOperator.EQ
+import io.micronaut.context.annotation.Value
 import jakarta.inject.Singleton
+import uk.gov.justice.digital.backend.client.domain.DomainRegistryClient.Companion.Fields.DATA
+import uk.gov.justice.digital.backend.client.domain.DomainRegistryClient.Companion.Fields.PRIMARY_ID
+import uk.gov.justice.digital.backend.client.domain.DomainRegistryClient.Companion.Fields.SECONDARY_ID
+import uk.gov.justice.digital.backend.client.domain.DomainRegistryClient.Companion.Fields.TYPE
+import uk.gov.justice.digital.backend.client.domain.DomainRegistryClient.Companion.Indexes.SECONDARY_ID_TYPE_INDEX
 import uk.gov.justice.digital.model.Domain
-import java.util.UUID
+import java.util.*
 
-/**
- * TODO
- *  o figure out what this needs to do
- *  o will need to update an existing domain with the new one
- *  o may need to delete and insert - specifically where uuids are different which should normally be the case
- */
-
-// TODO - this could also be a repository. Review
 @Singleton
 class DomainRegistryClient(clientProvider: DynamoDBClientProvider) {
 
+    @Value("\${dpr.domainRegistry}")
+    private lateinit var domainRegistryName: String
+
     private val client: AmazonDynamoDB by lazy { clientProvider.client }
 
-    // TODO - determine what the publish process is and what operations are supported by the API
+    /**
+     * Publishes the specified domain in dynamodb with the following steps
+     *   o within a transaction
+     *      - delete existing domain record
+     *      - put specified domain
+     *
+     * This ensures that we can replace an existing domain with the specified domain without disrupting any running
+     * services that depend on that domain being present.
+     */
     fun publish(d: Domain) {
+        // Get existing domain records and create deletion items for them.
+        val existingDomainEntries = getExistingDomainEntries(d.name)
+        val existingDomainDeletions = existingDomainEntries.map {
+            TransactWriteItem().withDelete(
+                Delete()
+                    .withTableName(domainRegistryName)
+                    .withKey(it.filterKeys { key -> listOf(PRIMARY_ID, SECONDARY_ID).contains(key) })
+            )
+        }
 
-        // TODO -  can we do an in-place update
+        // Create a put item for the domain we are publishing.
+        val putWriteItem = TransactWriteItem()
+            .withPut(
+                Put()
+                    .withTableName(domainRegistryName)
+                    .withItem(
+                        mapOf(
+                            PRIMARY_ID to AttributeValue().withS(d.id.toString()),
+                            SECONDARY_ID to AttributeValue().withS(d.name),
+                            TYPE to AttributeValue().withS("domain"),
+                            // TODO - this needs to pass in the JSON string representing the domain
+                            DATA to AttributeValue().withS("""
+                                { "foo": "${UUID.randomUUID()} }"
+                            """.trimIndent())
+                        )
+                    )
+            )
 
+        // TODO - do we need any atrributes from the result?
+        val result = client.transactWriteItems(TransactWriteItemsRequest()
+            .withTransactItems(existingDomainDeletions + putWriteItem))
     }
 
-}
-
-// Evolve an approach to replacing an existing domain without causing disruption to any services querying the registry.
-object DynTest {
-
-    val client: AmazonDynamoDB by lazy {
-        AmazonDynamoDBClientBuilder.standard()
-            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(
-                "https://dynamodb.eu-west-2.amazonaws.com",
-                "eu-west-2"
-            ))
-            .build()
-    }
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println("Attempting domain update")
-
-        val tableName = "dpr-domain-registry-development"
-        val domainName = "Domain 114"
-
-        val queryRequest = QueryRequest()
-            .withTableName(tableName)
-            .withIndexName("secondaryId-type-index")
+    // We should only ever see a single entry for a given domain name. Supporting > 1 entries ensures that we can clean
+    // up any unexpected state as part of the publish process.
+    private fun getExistingDomainEntries(domainName: String): List<Map<String, AttributeValue>> {
+        val existingDomainQuery = QueryRequest()
+            .withTableName(domainRegistryName)
+            .withIndexName(SECONDARY_ID_TYPE_INDEX)
             .withKeyConditions(
                 mapOf(
-                    "secondaryId" to Condition()
+                    // TODO - we can use extension methods here
+                    PRIMARY_ID to Condition()
                         .withAttributeValueList(AttributeValue().withS(domainName))
                         .withComparisonOperator(EQ),
-                    "type" to Condition()
+                    TYPE to Condition()
                         .withAttributeValueList(AttributeValue().withS("domain"))
                         .withComparisonOperator(EQ),
                 )
             )
 
-        val r2 = client.query(queryRequest)
+        return client
+            .query(existingDomainQuery)
+            .items
+    }
 
-        val deleteWriteItems = r2.items.map {
-            TransactWriteItem().withDelete(
-                Delete()
-                    .withTableName(tableName)
-                    .withKey(it.filterKeys { key -> listOf("primaryId", "secondaryId").contains(key) })
-                )
+    companion object {
+
+        object Fields {
+            const val PRIMARY_ID = "primaryId"
+            const val SECONDARY_ID = "secondaryId"
+            const val TYPE = "type"
+            const val DATA = "data"
         }
 
-        println("Query returned: ${r2.count} results")
+        object Indexes {
+            const val PRIMARY_ID_TYPE_INDEX = "primaryId-type-index"
+            const val SECONDARY_ID_TYPE_INDEX = "secondaryId-type-index"
+        }
 
-        val existingDomain = r2.items[0]
+        object Types {
+            const val DOMAIN = "domain"
+            // Not currently used - see DPR-218 for the intent here.
+            const val TABLE_SOURCE = "tablesource"
+        }
 
-        val existingDomainId = existingDomain["primaryId"]?.s
-
-        println("Existing domain has ID: $existingDomainId")
-
-        val putWriteItem = TransactWriteItem()
-            .withPut(
-                Put()
-                    .withTableName(tableName)
-                    .withItem(
-                        mapOf(
-                            "primaryId" to AttributeValue().withS(UUID.randomUUID().toString()),
-                            "secondaryId" to AttributeValue().withS(domainName),
-                            "type" to AttributeValue().withS("domain"),
-                            "data" to AttributeValue().withS("""
-                                { "foo": "${UUID.randomUUID()} }"
-                            """.trimIndent())
-                        )
-                    )
-        )
-
-        val result = client.transactWriteItems(TransactWriteItemsRequest()
-            .withTransactItems(deleteWriteItems + putWriteItem))
-
-        println("Got reuslt: $result")
     }
+
 }
